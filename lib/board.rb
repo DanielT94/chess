ef8df82a4bb5dc64684633e625480a90747c0cd0
DanielT94 <daniel.turnbull94@gmail.com>
2022-12-06 15:29:11 +0000

    Updated board to display pieces

--- lib/board.rb
+++ lib/board.rb
@@ -1,21 +1,37 @@
-require_relative './lib/modules/colours'
+require_relative 'modules/colours'
+require_relative 'modules/start_position'
+require_relative 'pieces/pawn'
+require_relative 'pieces/rook'
+require_relative 'pieces/knight'
+require_relative 'pieces/bishop'
+require_relative 'pieces/queen'
+require_relative 'pieces/king'
 
 class Board
   include Colours
+  include StartPosition
+
+  attr_reader :board
 
   def initialize
     @board = Array.new(8) { Array.new(8) }
   end
   
   def display_board
-    puts 'a b c d e f g h'
+    puts ' a  b  c  d  e  f  g  h'
+    chequered_board.size.times do |idx|
+      puts chequered_board[idx].join
+    end
+    puts ' a  b  c  d  e  f  g  h'
   end
 
   def chequered_board
     board.map.with_index do |row, row_idx|
       row.map.with_index do |square, square_idx|
         if square.nil?
-          paint_empty_squares
+          paint_empty_squares(row_idx, square_idx)
+        else
+          paint_populated_squares(row_idx, square_idx, square)
         end
       end
     end
@@ -27,6 +43,27 @@ class Board
     else
       bg_light_yellow('   ')
     end
+  end
+
+  def paint_populated_squares(row_idx, square_idx, square)
+    if (row_idx - square_idx).abs.odd?
+      bg_light_green(" #{square.symbol} ")
+    else
+      bg_light_yellow(" #{square.symbol} ")
+    end
+  end
+
+  def place(piece, file, rank)
+    board[rank][file] = piece
+  end
+
+  def populate_board
+    place_pawns
+    place_rooks
+    place_knights
+    place_bishops
+    place_queens
+    place_kings
   end
 end
 
